4011b54050eda1060d954dd88802835b316d2fff
BM Hudson <ttnhan@brickmate.vn>
2023-12-18 15:47:29 +0700

    update seal svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { Flex, List } from 'antd';
-import { Canvg, RenderingContext2D } from 'canvg';
-import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import './App.css';
 import SealDemo from './SealDemo';
-import seal1 from './assets/seal-1.svg';
 
 const data: { type: 'star' | 'dot'; name: string; bold: boolean }[] = [
 	{ type: 'dot', name: '박', bold: false },
@@ -53,36 +50,6 @@ const data: { type: 'star' | 'dot'; name: string; bold: boolean }[] = [
 ];
 
 function App() {
-	const canvasRef = useRef<HTMLCanvasElement>(null);
-	const [v, setV] = useState<Canvg>();
-	const [comName, setComName] = useState<string>('');
-
-	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-		// 👇 Store the input value to local state
-		setComName(e.target.value);
-	};
-
-	const drawSeal = async (canvas: HTMLCanvasElement) => {
-		const ctx = canvas.getContext('2d');
-		const vRender = await Canvg.from(ctx as RenderingContext2D, seal1);
-		setV(vRender);
-	};
-
-	useEffect(() => {
-		if (canvasRef.current) {
-			const canvas = canvasRef.current;
-			drawSeal(canvas);
-		}
-	}, [canvasRef]);
-
-	useEffect(() => {
-		v?.start();
-
-		return () => {
-			v?.stop();
-		};
-	}, [v]);
-
 	return (
 		<div>
 			<Flex gap={100} vertical>
